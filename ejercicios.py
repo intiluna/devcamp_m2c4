ea5e3759cde320e43b509feb25a49dd5a7e9a32a
Inti Luna <inlua@alumni.uv.es>
2025-03-10 17:08:38 +0100

    ejercicio 1

---
+++ ejercicios.py
@@ -0,0 +1,27 @@
+# Exercise 1: Create a list, tuple, float, integer, decimal, and dictionary.
+print("Exercise 1: Create a list, tuple, float, integer, decimal, and dictionary.")
+# List
+mi_list = [1, 2, 3, 4, 5]
+print(mi_list)
+
+# Tuple
+mi_tuple = ("important", "danger", "not try to change", "immutable")
+print(mi_tuple) 
+
+# Float
+mi_float = 41.2
+print(mi_float)
+
+# Integer
+mi_integer = 41
+print
+
+# Decimal
+from decimal import Decimal
+mi_decimal = Decimal('41.2')
+print(mi_decimal)
+
+# Dictionary
+mi_dict = {'name': 'Kathy', 'age': 5, 'city': 'Managua'}
+print(mi_dict)
+print("------------------------------------")
